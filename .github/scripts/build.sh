c801d23e65386cb44009dc0f487607a329a5713e
Adam Reichold <adam.reichold@t-online.de>
2021-09-28 08:25:54 +0200

    Replace Travis CI by GitHub Actions.

---
+++ .github/scripts/build.sh
@@ -0,0 +1,15 @@
+sudo apt-get install --yes --no-install-recommends qtbase5-dev qt5-qmake libqt5svg5-dev qt5-default
+
+mkdir -p build
+pushd build
+
+qmake ..
+make -j$(nproc)
+make INSTALL_ROOT=../appdir install
+
+popd
+
+wget --continue --no-verbose "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
+VERSION=`git rev-parse --short HEAD` ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/QMediathekView.desktop -appimage -extra-plugins=iconengines
